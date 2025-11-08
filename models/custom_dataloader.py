c6d76e021a6295de52ba0d729ecdf398189fba06
kevinbalderas01 <balderaskevin01@gmail.com>
2025-11-08 10:29:19 -0600

    update models and files

--- models/custom_dataloader.py
+++ models/custom_dataloader.py
@@ -1,14 +1,15 @@
 import torch
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoTokenizer, AutoModel
+from torch.utils.data import Dataset
 
 class CustomProteinDataset(Dataset):
     def __init__(self, path):
         df = pd.read_csv(path)
         self.sequences = [row.replace("/","") for row in df['seq'].tolist()] #Se concatena ambas cadenas como una sola
         plddt_scores = df['plddt'].astype(float)
+        iptm_scores = df['i_ptm'].astype(float)
         self.plddts = torch.tensor(plddt_scores.values, dtype=torch.float32).view(-1,1)
+        self.iptms = torch.tensor(iptm_scores.values, dtype=torch.float32).view(-1,1)
 
     def __len__(self):
         return len(self.sequences)
@@ -16,4 +17,5 @@ class CustomProteinDataset(Dataset):
     def __getitem__(self, index):
         sequence = self.sequences[index]
         plddt_score = self.plddts[index]
-        return sequence, plddt_score
+        iptm_score = self.iptms[index]
+        return sequence, plddt_score, iptm_score
--- models/mlp.py
+++ models/mlp.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 class MLP_pLDDT(nn.Module):
     def __init__(self, input_size, hidden_sizes, output_size):
@@ -11,21 +12,10 @@ class MLP_pLDDT(nn.Module):
             layers.append(nn.Dropout(0.2))
             prev_size = hidden_size
         layers.append(nn.Linear(prev_size, output_size))
+        layers.append(nn.Sigmoid())
         self.network = nn.Sequential(*layers)
 
     def forward(self, embedding_A):
         #x = torch.cat((embedding_A, embedding_B), dim=1)
         output = self.network(embedding_A)
         return output
-
-#input_size = 1024
-#hidden_sizes = [1024,512,254]  # 3 capas ocultas
-#output_size = 1
-
-#model = MLP_pLDDT(input_size, hidden_sizes, output_size)
-#print(model)
-
-#criterion = nn.MSELoss()  # Mean Squared Error para regresión
-#optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-
